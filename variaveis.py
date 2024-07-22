547a12c5714cd5cbbd877cd40a6cc2dfee69e972
victorkelvin <victorkelvin@gmail.com>
2024-07-22 13:16:04 -0300

    update variaveis

--- variaveis.py
+++ variaveis.py
@@ -10,6 +10,10 @@ class DevelopmentConfig():
 class ProductionConfig():
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://iscalabrini:IScalabrini@localhost:5432/iscalabrini_db'
+    CELERY_BROKER_URL = 'amqp://localhost:5672'
+    CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
+
+
 
 
 
