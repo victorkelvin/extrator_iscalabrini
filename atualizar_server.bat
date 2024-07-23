7cea60111a277b064adb0f38e6cf2d58776ce4ca
victorkelvin <victorkelvin@gmail.com>
2024-07-23 00:18:23 -0300

    update bat scripts

---
+++ atualizar_server.bat
@@ -0,0 +1,29 @@
+@echo
+
+:: Set the repository URL and local path
+set REPO_URL=https://github.com/victorkelvin/extrator_iscalabrini.git
+::set LOCAL_PATH=C:\Path\To\Your\Repo
+
+:: Change to the local repository path
+::cd /d %LOCAL_PATH%
+
+:: Checkout the repository if it doesn't exist
+if not exist .git (
+    git init
+    git remote add origin %REPO_URL%
+    git pull origin main
+) else (
+    :: Update the repository if it already exists
+    git pull origin main
+)
+
+call .venv\Scripts\activate.bat
+pip install -r requirements.txt
+
+start celery -A app worker --pool=solo -l INFO
+
+
+if %errorlevel% neq 0 (
+    echo Error occurred. Press any key to exit...
+    pause >nul
+)
